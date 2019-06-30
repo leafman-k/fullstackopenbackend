0929e689a2796ad34b22c276ee0831c0005a93ec
Lofman <kimmo.lofman@gmail.com>
2019-06-30 13:47:00 +0300

    Added eslint config

--- models/person.js
+++ models/person.js
@@ -1,29 +1,31 @@
+/* eslint-disable no-undef */
 const mongoose = require('mongoose')
 mongoose.set('useFindAndModify', false)
-const uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require('mongoose-unique-validator')
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
-mongoose.connect(url, {useNewUrlParser: true})
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+mongoose.connect(url, { useNewUrlParser: true })
+// eslint-disable-next-line no-unused-vars
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const phonebookSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true, minlength: 3},
-    number: { type: String, required: true, minlength: 8}
+  name: { type: String, required: true, unique: true, minlength: 3 },
+  number: { type: String, required: true, minlength: 8 }
 })
 
 phonebookSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 phonebookSchema.plugin(uniqueValidator)
 module.exports = mongoose.model('Person', phonebookSchema)
--- mongo.js
+++ mongo.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-undef */
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  // eslint-disable-next-line no-undef
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -13,33 +15,34 @@ const number = process.argv[4]
 const url =
     `mongodb+srv://trainer:${password}@fullstackopen-mvn2c.mongodb.net/phonebook-app?retryWrites=true`
 
-mongoose.connect(url, {useNewUrlParser: true})
+mongoose.connect(url, { useNewUrlParser: true })
 
 const phonebookSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', phonebookSchema)
 
 
 if (process.argv.length === 3) {
-    Person.find({}).then(result => {
-        console.log('Phonebook:')
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    console.log('Phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+  })
 } else {
-    const person = new Person({
-        name: name,
-        number: number,
-    })
-    person.save().then(response => {
-        console.log(`Added ${person.name} number ${person.number} to phonebook`)
-        mongoose.connection.close()
-    })
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+  // eslint-disable-next-line no-unused-vars
+  person.save().then(response => {
+    console.log(`Added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 
 
